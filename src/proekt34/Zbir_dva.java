4f783458f1c8527a6362230cd8871e36cf757f2b
unknown <simonastroimanova@hotmail.com>
2024-01-20 18:27:54 +0100

    first java program

---
+++ src/proekt34/Zbir_dva.java
@@ -0,0 +1,15 @@
+package proekt34;
+
+public class Zbir_dva {
+	static void Zbir() {
+		int a=3;
+		int b=6;
+		int c;
+		c=a+b;
+		System.out.print("A=3 , B=6 , C=A+B odnosno C =");
+		System.out.print(c);
+	}
+	public static void main(String[] args) {
+		Zbir();
+	}
+}
